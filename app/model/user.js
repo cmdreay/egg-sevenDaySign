f15e4a5559bc33fe673fb5aa5e7e4de6db13ed05
cmdreay <cmdream2016@163.com>
2019-12-16 14:30:19 +0800

    init

--- app/model/user.js
+++ app/model/user.js
@@ -1,1 +1,15 @@
 'use strict';
+
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const UserSchema = new mongoose.Schema(
+    {
+      username: String, // 用户名
+      password: String, // 用户密码
+      crewtedDate: Date, // 创建时间
+      score: Number // 积分
+    },
+    { versionKey: false }
+  );
+  return mongoose.model('User', UserSchema);
+};
